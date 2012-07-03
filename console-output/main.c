7ed498a3faeac7d9953f8b6fe70e0ee90c0f019b
Chen Xu <chen@c-2games.com>
2012-07-02 17:32:33 -0700

    add hello

---
+++ console-output/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  console-output
+//
+//  Created by C-2games on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
